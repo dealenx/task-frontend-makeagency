d2fa3aa8ffe4af9835468e56cfa74ce55d6f9f45
dealenx <dealenx@gmail.com>
2019-08-17 01:18:43 +0700

    Настроил деплой в gh-pages для демо-версии

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,2 @@
-npm run build
-
-cd dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-
-git push -f git@github.com:delaenx/task-frontend-makeagency.git master:gh-pages
-
-cd -
+@echo off
+npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/dealenx/task-frontend-makeagency.git master:gh-pages&&cd ..
