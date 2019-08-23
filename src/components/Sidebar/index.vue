277b730d0cc9a36ede77bdcac8fe6ad182665057
dealenx <dealenx@gmail.com>
2019-08-24 03:56:43 +0700

    Переписал сайдбар, добавил emoji

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,17 +1,12 @@
 <template>
   <ul class="sidebar-ul" v-bind:class="{ 'stick': scrollY > 175 }">
-    <li class="active">
-      <a href="#">Напутственное слово</a>
+    <li v-for="(item,index) in anchorLinks" :key="index" ref="acnhorLink" :data-index="index">
+      <a v-bind:href="'#'+item.anchor+''">{{item.title}}</a>
     </li>
+    <!-- 
     <li>
-      <a href="#">Сетка</a>
-    </li>
-    <li>
-      <a href="#">Типографика</a>
-    </li>
-    <li>
-      <a href="#">UI</a>
-    </li>
+      <a href="#ui">UI</a>
+    </li>-->
   </ul>
 </template>
 
@@ -23,15 +18,70 @@ export default {
       scrollY: 0
     };
   },
+  props: {
+    anchorLinks: {
+      default: [{ title: "Напутственное слово", anchor: "desc" }]
+    }
+  },
   mounted() {
     this.getScrollY();
+    this.followingAnchors();
   },
   methods: {
     getScrollY() {
       let self = this;
       document.getElementById("app").addEventListener("scroll", function() {
         self.scrollY = document.getElementById("app").scrollTop;
+        self.followingAnchors();
       });
+    },
+    followingAnchors() {
+      let self = this;
+
+      let anchors = document.querySelectorAll("a[name]");
+
+      let min = 10000;
+      let minIndex = 0;
+
+      /*for (let i = 0; i < anchors.length; i++) {
+        let top = Math.abs(anchors[i].offsetTop - self.scrollY);
+        let bottom = Math.abs(
+          anchors[i].offsetTop + anchors[i].offsetWidth - self.scrollY
+        );
+        let minValue = top < bottom ? top : bottom;
+        if (min > minValue) {
+          min = minValue;
+          minIndex = i;
+        }
+      }*/
+      console.clear();
+      let currentIndex = 0;
+      for (let i = 0; i < anchors.length - 1; i++) {
+        if (
+          self.scrollY >= Math.abs(anchors[i].offsetTop) &&
+          self.scrollY < Math.abs(anchors[i + 1].offsetTop)
+        ) {
+          currentIndex = i;
+        } else if (self.scrollY >= Math.abs(anchors[i].offsetTop)) {
+          currentIndex = anchors.length - 1;
+        } else {
+          currentIndex = 0;
+        }
+      }
+
+      for (let item of this.$refs.acnhorLink) {
+        item.classList.remove("active");
+      }
+      this.$refs.acnhorLink[currentIndex].classList.add("active");
+
+      /*console.clear();
+      console.log(minIndex);
+      console.log(this.$refs.acnhorLink[minIndex]);
+      for (let item of this.$refs.acnhorLink) {
+        item.classList.remove("active");
+      }
+      this.$refs.acnhorLink[minIndex].classList.add("active");
+      console.log(min);*/
     }
   }
 };
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="site-home">
     <div class="site-home__sidebar">
-      <Sidebar />
+      <Sidebar
+        :anchor-links="
+      [
+        { title: 'Напутственное слово', anchor: 'desc' },
+        { title: 'Сетка', anchor: 'grid' },
+        { title: 'Типографика', anchor: 'typography' },
+        { title: 'UI', anchor: 'ui' }
+      ]
+      "
+      />
     </div>
     <div class="site-home__content">
       <div class="site-home__main-info">
+        <a class="anchor" name="desc"></a>
         <h1>Тестовое задание</h1>
         <h3>Уровень: junior</h3>
         <p>Привет! Твоё тестовое задание — грамотно заверстать эту прекрасную страничку. В шапке страницы укажи своё ФИО, так всем будет удобнее. В менюхе под иконкой бургером мы хотели бы также видеть твои данные. Ну что, поехали?</p>
       </div>
 
       <div class="site-section">
+        <a class="anchor" name="grid"></a>
         <h2>Сетка</h2>
         <p>Задание предполагает адаптивную вёрстку. Используется 3 состояния в зависимости от устройства. Сетка «резиновая» — переменной ширины.</p>
 
@@ -88,41 +99,187 @@
       </div>
 
       <div class="site-section">
+        <a class="anchor" name="typography"></a>
         <h2>Типография</h2>
         <p>Используется шрифт Graphik. На телефонах стили типографики переопределяются</p>
 
         <tabs class="site-subsection tabs" activeTab="tab-1">
           <ul class="site-tabs__nav">
-            <li tab-link="tab-1">
-              <a>Компьютер</a>
+            <li tab-link="tab-1">Компьютер</li>
+            <li tab-link="tab-2">Телефон</li>
+          </ul>
+
+          <ul class="site-typography" tab-content="tab-1">
+            <li>
+              <div class="site-typography__desc">Заголовок H1, 48/56, bold</div>
+              <div class="site-typography__example">
+                <h1 class="laptop">Текст заголовка</h1>
+              </div>
             </li>
-            <li tab-link="tab-2">
-              <a>Телефон</a>
+
+            <li>
+              <div class="site-typography__desc">Заголовок H2, 32/40, bold</div>
+              <div class="site-typography__example">
+                <h2 class="laptop">Текст заголовка</h2>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Заголовок H3, 24/32, medium</div>
+              <div class="site-typography__example">
+                <h3 class="laptop">Текст заголовка</h3>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Заголовок H4, 18/28, medium</div>
+              <div class="site-typography__example">
+                <h4 class="laptop">Текст заголовка</h4>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Лид P_Lead, 24/32, regular</div>
+              <div class="site-typography__example">
+                <p class="p-lead laptop">Текст лида</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Крупный текст P_18, 18/28, regular</div>
+              <div class="site-typography__example">
+                <p class="p-18 laptop">Крупный текст</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Основной текст P_16, 16/24, regular</div>
+              <div class="site-typography__example">
+                <p class="p-16 laptop">Основной текст</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Впомогательный текст P_14, 14/20, regular</div>
+              <div class="site-typography__example">
+                <p class="p-14 laptop">Вспомогательный текст</p>
+              </div>
             </li>
           </ul>
 
-          <div tab-content="tab-1">
-            <h2>Механическая очистка</h2>
-          </div>
+          <ul class="site-typography" tab-content="tab-2">
+            <li>
+              <div class="site-typography__desc">Заголовок H1, 48/56, bold</div>
+              <div class="site-typography__example">
+                <h1 class="mobile">Текст заголовка</h1>
+              </div>
+            </li>
 
-          <div tab-content="tab-2">
-            <h2>Пескоструйная обработка</h2>
-          </div>
+            <li>
+              <div class="site-typography__desc">Заголовок H2, 32/40, bold</div>
+              <div class="site-typography__example">
+                <h2 class="mobile">Текст заголовка</h2>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Заголовок H3, 24/32, medium</div>
+              <div class="site-typography__example">
+                <h3 class="mobile">Текст заголовка</h3>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Заголовок H4, 18/28, medium</div>
+              <div class="site-typography__example">
+                <h4 class="mobile">Текст заголовка</h4>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Лид P_Lead, 24/32, regular</div>
+              <div class="site-typography__example">
+                <p class="p-lead mobile">Текст лида</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Крупный текст P_18, 18/28, regular</div>
+              <div class="site-typography__example">
+                <p class="p-18 mobile">Крупный текст</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Основной текст P_16, 16/24, regular</div>
+              <div class="site-typography__example">
+                <p class="p-16 mobile">Основной текст</p>
+              </div>
+            </li>
+
+            <li>
+              <div class="site-typography__desc">Впомогательный текст P_14, 14/20, regular</div>
+              <div class="site-typography__example">
+                <p class="p-14 mobile">Вспомогательный текст</p>
+              </div>
+            </li>
+          </ul>
         </tabs>
       </div>
+
+      <div class="site-section">
+        <a class="anchor" name="ui"></a>
+        <h2>UI</h2>
+
+        <div class="site-subsection">
+          <h3>Текстовое поле</h3>
+          <div class="dev">
+            <h2>В разработке</h2>
+          </div>
+        </div>
+        <div class="site-subsection">
+          <h3>Выпадающее меню</h3>
+          <div class="dev">
+            <h2>В разработке</h2>
+          </div>
+        </div>
+        <div class="site-subsection">
+          <h3>Кнопка</h3>
+          <div class="dev">
+            <h2>В разработке</h2>
+          </div>
+        </div>
+        <div class="site-subsection">
+          <h3>Чекбокс, радиобаттон</h3>
+          <div class="dev">
+            <h2>В разработке</h2>
+          </div>
+        </div>
+        <div class="site-subsection">
+          <h3>Аккордеон</h3>
+          <div class="dev">
+            <h2>В разработке</h2>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="site-home__footer">Победа! 😜</div>
   </div>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar";
 import { Tabs } from "@/components/UI";
+import twemoji from "twemoji";
 
 export default {
   name: "Home",
   components: {
     Sidebar,
     Tabs
+  },
+  mounted() {
+    twemoji.parse(document.getElementsByClassName("site-home__footer")[0]);
   }
 };
 </script>
